68d13e38438dc39690b3cca03361f5f643498a2a
igorigorevich942 <igorigorevich942@rambler.ru>
2016-11-17 22:26:13 +0300

    Update m3_linux_task.sh

--- m3_linux_task.sh
+++ m3_linux_task.sh
@@ -1,2 +1,2 @@
-wget https://ru.msi.com/news; cat news|grep  '"news_title' >news1;cat news1|sed -e 's/<div class="news_title">//g' >news2;cat news2| sed -e 's/div>//g' >news1; cat news|grep  '"news_date' >news2;cat news2|sed -e 's/<div class="news_date">//g' >news3;cat news3| sed -e 's/div>//g' >news2;paste news2 : news1 >news5;  cat news5;rm news news1 news2 news3
+wget https://ru.msi.com/news; cat news|grep  '"news_title' >news1;cat news1|sed -e 's/<div class="news_title">//g' >news2;cat news2| sed -e 's/div>//g' >news1; cat news|grep  '"news_date' >news2;cat news2|sed -e 's/<div class="news_date">//g' >news3;cat news3| sed -e 's/div>//g' >news2;paste news2  news1 >news5;  cat news5;rm news; rm news1; rm news2; rm news3
 
